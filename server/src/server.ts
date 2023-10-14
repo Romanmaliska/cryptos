7c66780d88b5dc62419ffd619c0dd620cb792803
Roman Maliska <roman.maliska@firma.seznam.cz>
2023-10-14 17:03:02 +0200

    feat: register user

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,25 @@
-import bodyParser from "body-parser";
+import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import express, { Application, Request, Response } from "express";
-import { errorHandler, notFound } from "./middleware/errorMiddleware";
+
 import userRoutes from "./routes/userRoutes";
+
+import { errorHandler, notFound } from "./middleware/errorMiddleware";
+
 import connectDB from "./config/db";
 
 connectDB();
 
 const port = process.env.PORT || 5000;
 const app: Application = express();
+const userRoutePath = "/api/users";
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/users", userRoutes);
+app.use(userRoutePath, userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
-
 
 app.listen(port, () => console.log(`Server is running on PORT ${port}`));
