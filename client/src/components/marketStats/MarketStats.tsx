0a0a31810759340b22acc4b0ad785fa80adb2400
Roman Maliska <roman.maliska@firma.seznam.cz>
2023-12-26 17:17:34 +0100

    feat: marketStats component

--- client/src/components/marketStats/MarketStats.tsx
+++ client/src/components/marketStats/MarketStats.tsx
@@ -1,4 +1,43 @@
+import { useAppSelector } from "store/useAppDispatch";
+import { selectCurrencyRates } from "slices/currencySlice";
 
-export default function MarketStats() {
-  return <div>MarketStats</div>;
+import { parseCurrency } from "utils/parseCurrency";
+
+import { Stats } from "types/common";
+import styles from "components/marketStats/MarketStats.module.css";
+
+type Props = {
+  stats: Stats;
+};
+
+export default function MarketStats({ stats }: Props) {
+  const currencyState = useAppSelector(selectCurrencyRates);
+
+  const vulumeInSetCurrency = parseCurrency({
+    price: stats.total24hVolume,
+    currencyState,
+  });
+  const marketInSetCurrency = parseCurrency({
+    price: stats.totalMarketCap,
+    currencyState,
+  });
+
+  const labels = ["Volume 24h", "Market Cap"];
+
+  return (
+    <section className={styles.market_stats}>
+      {labels.map((label) => {
+        return (
+          <div key={label}>
+            <p>{label}</p>
+            <h2>
+              {label === "Volume 24h"
+                ? vulumeInSetCurrency
+                : marketInSetCurrency}
+            </h2>
+          </div>
+        );
+      })}
+    </section>
+  );
 }
